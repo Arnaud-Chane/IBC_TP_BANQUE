766a220bafd081615c3ad9edfc4d5889dc2effd6
Arnaud <arnaud.chane.dev@gmail.com>
2024-02-20 15:33:18 +0100

    Adding method with parameters overdraft and number of day

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
-import static org.example.agios.Agios.DisplayAgios;
+import static org.example.agios.Agios.*;
 
 public class Main {
     public static void main(String[] args) {
         DisplayAgios();
+        CalculAgios(15, 25);
     }
 }
--- src/main/java/org/example/agios/Agios.java
+++ src/main/java/org/example/agios/Agios.java
@@ -5,4 +5,10 @@ public class Agios {
     public static void DisplayAgios(){
         System.out.println("Agios");
     }
+
+    public static double CalculAgios(int overdraft, int dayOfOverdraft){
+        double DOverdraft = overdraft;
+        System.out.println("Overcraft : "+DOverdraft+" and Number of day of overdraft : "+dayOfOverdraft+".");
+        return DOverdraft;
+    }
 }
